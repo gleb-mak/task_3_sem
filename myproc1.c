f92350268f0d82cfe1859bf044a91963d026f93a
gleb-mak <57875990+gleb-mak@users.noreply.github.com>
2020-12-11 15:36:11 +0300

    Update myproc1.c

--- myproc1.c
+++ myproc1.c
@@ -10,19 +10,27 @@ int main(int argc, char* argv[])
     DIR *mydir;
     struct dirent *myfile;
     struct stat mystat;
-    int count = 0;
+    int count1 = 0;
+    int count2 = 0;
+    int count3 = 0;
     int isdir = 0;
     mydir = opendir("/proc");
     while ((myfile = readdir(mydir)) != NULL)
     {
         stat(myfile->d_name, &mystat);
         isdir = S_ISDIR(mystat.st_mode);
-        if ((myfile->d_name[0] >= '0') && (myfile->d_name[0] <= '9') && isdir)
+        if (isdir)
         {
-            printf(" %s\n", myfile->d_name);
-            count++;
+            count1++;
         }
+        if ((myfile->d_name[0] >= '0') && (myfile->d_name[0] <= '9'))
+        {
+            //printf(" %s\n", myfile->d_name);
+            count2++;
+        }
+        count3++;
     }
-    printf("%d\n", count);
+    printf("isdir: %d\t started with numbrer: %d\t all: %d\n", count1, count2, count3);
     closedir(mydir);
 }
+
